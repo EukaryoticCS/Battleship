826f66917292f2015db748ab361400cf568c7d12
DomianosPizza <damianl.official@gmail.com>
2023-01-18 13:35:24 -0700

    Start Mod2a
    Start Mod2a and doing first push to brach

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,2 +1,38 @@
 public class Gameplay {
+    private char[][] board;
+    private int rowSize;
+    private int columnSize;
+    public static  final char OPEN_SYMBOL = ' ';
+
+
+    public int getRowSize() {
+        return rowSize;
+    }
+
+    public int getColumnSize() {
+        return columnSize;
+    }
+
+    public void GameBoard(int columnSize, int rowSize) {
+        this.rowSize = rowSize;
+        this.columnSize = columnSize;
+        board = new char[rowSize][columnSize];
+    }
+    public void print() {
+        for (int row = 0; row < rowSize; row++) {
+            for (int column = 0; column < columnSize; column++) {
+                System.out.print(board[row][column]);
+                if (column < columnSize - 1) System.out.print("|");
+            }
+            if (row < rowSize - 1) {
+                System.out.println();
+                for (int column = 0; column < columnSize; column++) {
+                    System.out.println(" | ");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println();
+
+    }
 }
