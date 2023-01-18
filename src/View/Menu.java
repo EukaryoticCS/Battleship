d8bae53c70089301ee3053e3b9f4781a95188a09
Brandon Smith <98478090+EukaryoticCS@users.noreply.github.com>
2023-01-18 10:36:51 -0700

    Merged Menu.java

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -2,6 +2,19 @@ package View;
 
 public class Menu {
     MyInput input = new MyInput();
+    
+    public static void welcome(){
+        System.out.println(" Welcome to BattleShips");
+
+        System.out.println("                                  )___(");
+        System.out.println("                           _______/__/_");
+        System.out.println("                  ___     /===========|   ___");
+        System.out.println(" ____       __   [\\\\\\]___/____________|__[///]   __");
+        System.out.println(" \\   \\_____[\\\\]__/___________________________\\__[//]___");
+        System.out.println("  \\ 40A                  U.S.S Maine                   |");
+        System.out.println("   \\                                                  /");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 
     public void PlaceShips() {
 
