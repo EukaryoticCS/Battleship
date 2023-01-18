df69a778ac83dc5fd57dfb769ccd58cd80cc4b52
Damian Lagunas <dlagunas@student.neumont.edu>
2023-01-18 14:06:10 -0700

    fixing board to work :)

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,5 +1,5 @@
 public class Gameplay {
-    private char[][] board;
+    private static char[][] board = new char[10][10];
     private int rowSize;
     private int columnSize;
     public static  final char OPEN_SYMBOL = ' ';
@@ -12,27 +12,26 @@ public class Gameplay {
     public int getColumnSize() {
         return columnSize;
     }
-
-    public void GameBoard(int columnSize, int rowSize) {
-        this.rowSize = rowSize;
-        this.columnSize = columnSize;
-        board = new char[rowSize][columnSize];
-    }
     public void print() {
         for (int row = 0; row < rowSize; row++) {
+            System.out.print("| ");
             for (int column = 0; column < columnSize; column++) {
                 System.out.print(board[row][column]);
-                if (column < columnSize - 1) System.out.print("|");
-            }
-            if (row < rowSize - 1) {
-                System.out.println();
-                for (int column = 0; column < columnSize; column++) {
-                    System.out.println(" | ");
-                }
-                System.out.println();
+                if (column < columnSize - 1) System.out.print(" | ");
             }
         }
         System.out.println();
-
     }
+
+    public void InitializeBoard(char[][] board){
+        for (int row = 0; row < board[0].length; row++)
+        {
+            for (int col = 0; col < board[1].length; col++)
+            {
+                board[col][row] = '~';
+            }
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
         Gameplay game = new Gameplay();
-        game.GameBoard(3,3);
         game.print();
     }
 
