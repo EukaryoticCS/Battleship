b751c3edf25c566f756058100d5b97f5adc56784
Brandon Smith <98478090+EukaryoticCS@users.noreply.github.com>
2023-01-18 10:38:25 -0700

    Called welcome()

--- src/Controller/Gameplay.java
+++ src/Controller/Gameplay.java
@@ -5,6 +5,7 @@ import View.Menu;
 public class Gameplay {
     public static void main(String[] args) {
         Menu menu = new Menu();
+        menu.welcome();
         menu.PlaceShips();
     }
 }
