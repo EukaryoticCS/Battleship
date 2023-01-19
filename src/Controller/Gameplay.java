a63473ab80b5c1459af27fbd8920461c8c5a01e7
Damian Lagunas <dlagunas@student.neumont.edu>
2023-01-18 17:45:52 -0700

    Merge Main to Mod2a
    Merging main to the mod2a branch so that there are little commit issues

--- src/Controller/Gameplay.java
+++ src/Controller/Gameplay.java
@@ -5,7 +5,9 @@ import View.Menu;
 public class Gameplay {
     public static void main(String[] args) {
         Menu menu = new Menu();
+        menu.GameBoard(10,10);
         Menu.welcome();
         menu.PlaceShips();
+
     }
 }
