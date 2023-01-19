a643adb315f23c7515c1b71462708775eaf79a29
Damian Lagunas <dlagunas@student.neumont.edu>
2023-01-18 17:33:32 -0700

    finish the board to display a 10 x 10 grid

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,35 +1,21 @@
 public class Gameplay {
-    private static char[][] board = new char[10][10];
+    public static char[][] board;
     private int rowSize;
     private int columnSize;
-    public static  final char OPEN_SYMBOL = ' ';
 
-
-    public int getRowSize() {
-        return rowSize;
-    }
-
-    public int getColumnSize() {
-        return columnSize;
+    public void GameBoard(int columnSize, int rowSize) {
+        this.rowSize = rowSize;
+        this.columnSize = columnSize;
+        board = new char[rowSize][columnSize];
     }
     public void print() {
-        for (int row = 0; row < rowSize; row++) {
+        for (int row = 0; row < board[0].length; row++) {
             System.out.print("| ");
-            for (int column = 0; column < columnSize; column++) {
-                System.out.print(board[row][column]);
-                if (column < columnSize - 1) System.out.print(" | ");
+            for(int column = 0; column < board[1].length; column++){
+                System.out.print(board[row][column] = '~');
+                System.out.print(" | ");
             }
-        }
-        System.out.println();
-    }
-
-    public void InitializeBoard(char[][] board){
-        for (int row = 0; row < board[0].length; row++)
-        {
-            for (int col = 0; col < board[1].length; col++)
-            {
-                board[col][row] = '~';
-            }
+            System.out.println();
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         Gameplay game = new Gameplay();
+        game.GameBoard(10,10);
         game.print();
     }
 
