03ed6d2f3d0a1a0e78f5ce79719a72dea065934e
bsmithNeumont <brsmith@student.neumont.edu>
2023-01-22 21:34:45 -0700

    Finished GamePlayLoop, tested all aspects

--- src/Controller/Gameplay.java
+++ src/Controller/Gameplay.java
@@ -28,15 +28,16 @@ public class Gameplay {
         boolean shipBuilt;
 
         //For Testing
-//        gameplay.BuildShip(new int[]{2, 2}, false, 5, 'C',board); //C3, H
-//        gameplay.BuildShip(new int[]{9, 5}, false, 4, 'B',board); //J6, H
-//        gameplay.BuildShip(new int[]{4, 6}, true, 3, 'R',board); //E7, V
-//        gameplay.BuildShip(new int[]{4, 8}, true, 3, 'S',board); //E9, V
-//        gameplay.BuildShip(new int[]{5, 4}, false, 2, 'D',board); //F5, H
+        BuildShip(new int[]{2, 2}, false, 5, 'C', Menu.aiBoard); //C3, H
+        BuildShip(new int[]{9, 5}, false, 4, 'B', Menu.aiBoard); //J6, H
+        BuildShip(new int[]{4, 6}, true, 3, 'R', Menu.aiBoard); //E7, V
+        BuildShip(new int[]{4, 8}, true, 3, 'S', Menu.aiBoard); //E9, V
+        BuildShip(new int[]{5, 4}, false, 2, 'D', Menu.aiBoard); //F5, H
 
-        Menu.DisplayBoard();
         for (int i = 0; i < ships.length; i++) { //Loop through each ship
             do {
+                Menu.DisplayBoard();
+
                 //Take in coordinate
                 System.out.println("Where would you like to place your " + ships[i].getShipName() + "? Enter a coordinate (e.g. D5):");
                 int[] inputCoordinate = input.GetCoords();
@@ -56,19 +57,15 @@ public class Gameplay {
                 //Take in 'V' or 'H', build ship accordingly
                 //"V" = true, "H" = false
 
-                shipBuilt = BuildShip(inputCoordinate, vertical, ships[i].getShipLength(), ships[i].getShipLetter(), Menu.board);
+                shipBuilt = BuildShip(inputCoordinate, vertical, ships[i].getShipLength(), ships[i].getShipLetter(), Menu.playerBoard);
                 if (!shipBuilt) {
                     System.out.println("That ship won't fit there! Try a different coordinate or direction.");
-                } else {
-                    Menu.DisplayBoard(); //Shows the player the board after placing the ship
                 }
             } while (!shipBuilt);
         }
     }
 
-
     public static boolean BuildShip(int[] coordinate, boolean vertical, int shipLength, char shipChar, char[][] board) {
-
 //        System.out.println("coordinate: (" + coordinate[0] + ", " + coordinate[1] + ") vertical: " + vertical);
         if (vertical) {
             if (coordinate[0] + shipLength <= 10) {
@@ -103,21 +100,17 @@ public class Gameplay {
         return false;
     }
 
-
     public static boolean HitShip(char[][] board, int[] coords) {
         if (/*the coordinate hits something that isn't a tilde it has hit a ship*/ board[coords[0]][coords[1]] != '~') { //if the board index at the coordinate is anything other than a tilde
-
             return true;
         }
         return false;
     }
 
-
     public static boolean SunkShip(char[][] board, char shipChar) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) {
                 if (board[row][col] == shipChar)
-
                     return false; //If it found any of the hit character
             }
         }
@@ -134,25 +127,27 @@ public class Gameplay {
     }
 
     public static boolean AITurn() { //Very similar to PlayerTurn()
-        char[] rows = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        char[] rows = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
         int[] coords;
         char shipChar;
+
         do {
             int row = random.nextInt(0, 9);
             int col = random.nextInt(0, 9);
 
-            coords = new int[]{row,col};
+            coords = new int[]{row, col};
+            shipChar = Menu.playerBoard[coords[0]][coords[1]];
 
-            shipChar = Menu.board[coords[0]][coords[1]];
         } while (shipChar == 'X' || shipChar == 'O');
 
-        System.out.printf("The AI attacks %c%d", rows[coords[0]],coords[1]);
+        System.out.printf("The AI attacks %c%d\n", rows[coords[0]], coords[1] + 1);
 
-        if (HitShip(Menu.board, coords)) {
+        if (HitShip(Menu.playerBoard, coords)) {
             System.out.println("The AI hit your " + getShipNameFromChar(shipChar) + "!");
-            Menu.board[coords[0]][coords[1]] = 'X';
+            Menu.playerBoard[coords[0]][coords[1]] = 'X';
 
-            if (SunkShip(Menu.board, shipChar)) { //Check for sink
+            if (SunkShip(Menu.playerBoard, shipChar)) { //Check for sink
                 System.out.println("The AI sunk your " + getShipNameFromChar(shipChar) + "!");
                 playerShipsSunk++;
 
@@ -163,17 +158,19 @@ public class Gameplay {
             }
         } else {
             System.out.println("The AI Missed!");
-            Menu.board[coords[0]][coords[1]] = 'O';
+            Menu.playerBoard[coords[0]][coords[1]] = 'O';
         }
-        return true;
+        return false;
     }
 
-    public void GamePlayLoop() {
-            boolean quit;
+    public static void GamePlayLoop() {
+        boolean quit;
+        do {
+            Menu.InitializeBoards();
+            PlaceShips();
             boolean validInput;
-            do {
-                while (true) {
-
+            while (true) {
+                Menu.DisplayBoard();
                 if (Menu.PlayerTurn()) { //Returns true if player won
                     break;
                 }
@@ -181,8 +178,7 @@ public class Gameplay {
                     break;
                 }
             }
-            do
-            {
+            do {
                 String playAgain = input.GetUserStr("Play again? (Y/N)", true);
                 validInput = playAgain.equals("Y") || playAgain.equals("N");
                 if (playAgain.equals("Y")) {
@@ -193,9 +189,7 @@ public class Gameplay {
                     //quit
                     quit = true;
                 }
-
-            }while(!validInput);
+            } while (!validInput);
         } while (!quit);
     }
-
 }
--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -4,16 +4,7 @@ import View.Menu;
 
     public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        Gameplay gameplay = new Gameplay();
-        boolean quit = false;
-
-        menu.InitializeBoard(); //Initialize the gameboard
         Menu.welcome();
-        gameplay.PlaceShips();
-
-
-
-
+        Gameplay.GamePlayLoop();
     }
 }
